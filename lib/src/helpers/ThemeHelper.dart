b3be6449506d5af312ba8184da8acbecffbf7c70
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-08-28 18:24:57 +0800

    Added ThemeHelper.dart

---
+++ lib/src/helpers/ThemeHelper.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class ThemeHelper {
+
+  static const Color primaryColor = Color(0xFF6C63FF);
+  static const Color secondaryColor = Color(0xFFFF6584);
+  static const Color backgroundColor = Color(0xFFF5F5F5);
+  static const Color textColor = Color(0xFF333333);
+  static const Color accentColor = Color(0xFF00C853);
+  static const Color buttonColor = Color(0xFF6C63FF);
+  static const Color borderColor = Color(0xFFDDDDDD);
+}
